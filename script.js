885137f33f90ebedbc2a095971656fc95cf41344
Hannah Rosen <hannahfayerosen@gmail.com>
2017-07-10 21:43:47 -0400

    Grid created, top margin filter working.

--- script.js
+++ script.js
@@ -1,17 +1,115 @@
+//function to hold the delete task function
+function createOnClickListenerRemove() {
+$(".ui-icon-close").click(function(){
+    $(this).parent().remove();
+});
+};
+
+
+createOnClickListenerRemove();  
+
+
 //sortable and draggable 
 $( function() {
     $( "#sortable" ).sortable({
-      revert: true,
-      cancel: ".ui-state-disabled"
+      revert: true
     });
     $( ".draggable" ).draggable({
       connectToSortable: "#sortable",
       helper: "clone",
       revert: "invalid",
       stack: "#sortable li", 
-    }).addClass("breadcrumbs");
+      start: function(e, ui){
+        $(ui.helper).addClass("breadcrumbs").children().removeClass("ui-icon-grip-dotted-vertical").addClass("ui-icon-close");
+        createOnClickListenerRemove();},
+        
+     stop: function(e, ui){
+        var selector = $(ui.helper).text();
+        if (selector == "Margin Top 20%"){
+            keepMarginTop();
+        }
+     }
+    });
+    
     $( "ul, li" ).disableSelection();
+    
   } );
+
+//doing stuff with JSON
+
+function proofData() {
+    console.log(data.ProductData[0].Vendor);
+}
+
+proofData();
+
+var SKU
+var ProductName
+var Picture
+var SubCat
+var Color
+var Style
+var Wash
+var Vendor
+var MarginNum
+var MarginGroup
+var VolumeNum
+var VolumeGroup
+
+function fillGrid (){
+    $(data.ProductData).each( function (i){
+        SKU = data.ProductData[i].SKU;
+        ProductName = data.ProductData[i].ProductName;
+        Picture = data.ProductData[i].Picture;
+        SubCat = data.ProductData[i].SubCat;
+        Color = data.ProductData[i].Color;
+        Style = data.ProductData[i].Style;
+        Wash = data.ProductData[i].Wash;
+        Vendor = data.ProductData[i].Vendor;
+        MarginNum = data.ProductData[i].MarginNum;
+        MarginGroup = data.ProductData[i].MarginGroup;
+        VolumeNum = data.ProductData[i].VolumeNum;
+        VolumeGroup = data.ProductData[i].VolumeGroup;
+        appendComponent();
+})
+}
+
+//helper function to construct component 
+function appendComponent(){
+    var productImage = $('<img>'); 
+    productImage.attr('src', Picture);
+    var title = $('<p class="productName"></p>').text(ProductName);
+    var skuNum = $('<p></p>').text("SKU# " + SKU);
+    var margin = $('<p></p>').text("Margin %: " + MarginNum);
+    $('<li class="gridTile"></li>').appendTo("#productGrid").append(productImage, title, skuNum, margin);
+}
+
+fillGrid();
+
+function keepMarginTop(){
+    $(".gridTile").remove();
+    $(data.ProductData).each( function (i){
+        if (data.ProductData[i].MarginGroup == "Top"  ){
+        SKU = data.ProductData[i].SKU;
+        ProductName = data.ProductData[i].ProductName;
+        Picture = data.ProductData[i].Picture;
+        SubCat = data.ProductData[i].SubCat;
+        Color = data.ProductData[i].Color;
+        Style = data.ProductData[i].Style;
+        Wash = data.ProductData[i].Wash;
+        Vendor = data.ProductData[i].Vendor;
+        MarginNum = data.ProductData[i].MarginNum;
+        MarginGroup = data.ProductData[i].MarginGroup;
+        VolumeNum = data.ProductData[i].VolumeNum;
+        VolumeGroup = data.ProductData[i].VolumeGroup;
+        appendComponent();
+        }
+    }
+    )
+}
+
+//keepMarginTop();
+
 
 
 /*$(function () {
@@ -54,7 +152,7 @@ function openTab(evt, tabName) {
 }
 
 function handleDrop(event, ui) {
-console.log("dropped");
+    console.log('dropped');
 }
 
 // Get the element with id="defaultOpen" and click on it
